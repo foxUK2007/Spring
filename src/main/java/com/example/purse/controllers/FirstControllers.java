61397228d2298dcc6ec38d43af2691c9fb5a653d
fox <zem_otsp@mail.ru>
2022-12-30 00:21:47 +0500

    ДЗ по Spring после замечаний

--- src/main/java/com/example/purse/controllers/FirstControllers.java
+++ src/main/java/com/example/purse/controllers/FirstControllers.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstControllers {
 
     @GetMapping
-
-    public String HelloFriend(){
+    public String helloFriend() {
         return "Приложение запущено";
     }
 
-    @GetMapping("/path/to/page")
-
-    public String Page(){
-        return Info();
-    }
-
-    public String Info(){
+    @GetMapping("/path/to/info")
+    public String info() {
         return "1. Сутягин А.В.; \n2. Первый проект; \n3. Создан 29.12.2022; \n4. Пилотный проект, созданный преимущественно при помощи русского мата.";
     }
+
 }
